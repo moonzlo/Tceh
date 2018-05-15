2bdee7040c5cea3dccc3cf40ae6baf711725209e
Mikhail <35227454+moonzlo@users.noreply.github.com>
2018-05-15 11:20:40 +0300

    Исправил ошибки

--- homeWork/FlaskDZ.py
+++ homeWork/FlaskDZ.py
@@ -1,65 +1,51 @@
+
 from flask import Flask, request
-from flask_wtf import FlaskForm
-from wtforms import SelectField, validators
 from flask.json import jsonify
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators, ValidationError
+
 
 app = Flask(__name__)
-app.debug = True
-
-class ContactForm(FlaskForm):
-    name = SelectField(validators=[validators.Length(min=4, max=25)])
-    email = SelectField(validators=[validators.Length(min=6,max=35),validators.Email])
-
-
-# Не мое решение (так как вообще задачу не понял) 
-@app.route('/locales', methods =['GET', 'POST'])
-def my_dict():
-    my_locale = {'ru': 'russian', 'en': 'english', 'it': 'italian'}
-    return jsonify(my_locale)
+app.config.update({
+    'SECRET_KEY': 'asdasdasd',
+    'DEBUG': True,
+    'WTF_CSRF_ENABLED': False
+})
 
 
+def confirm_password(form, field):
+    if form.data['password'] != form.data['confirm_password']:
+        raise ValidationError('Пароли не совпадают"')
 
 
+class UserForm(FlaskForm):
+    email = StringField(label='E-mail', validators=[
+        validators.Length(min=5, max=35), validators.Email()
+    ])
+    password = StringField(label='password:', validators =[
+        validators.Length(min=6, max=12)
+    ])
+    confirm_password = StringField(label='confirm password:', validators=[
+        validators.Length(min=6, max=20), confirm_password
+    ])
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
+
+@app.route('/form/user', methods =['GET', 'POST'])
+def post_data():
     if request.method == 'POST':
-        print(request.form)
-        form = ContactForm(request.form)
-        print(form.validate())
-
-        if form.validate():
-            return ('Всё забись', 200)
+        user_form = UserForm(request.form)
+        status_output = {0:'Проверка пройдена', 1: 'Ошибка валидации'}
+        if user_form.validate():
+            # print('email:', request.form['email'])
+            # print('pass:', request.form['password'])
+            status_check = jsonify(status_output[0])
+            return status_check
         else:
-            return ('ОШИБКА', 400)
+            status_check = jsonify(status_output[1])
+            error_list = jsonify(user_form.errors)
+            # print(user_form.errors)
+            return status_check and error_list
+    return 'Done!'
 
-    if request.method == 'GET':
-        return 'HELLO MOTHERFUCKER', 200
-
-
-@app.route('/serve/<path:filename>')
-def patch(filename):
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            text = file.readlines()
-            return str(text)
-
-    except Exception as error:
-        return 'Ошибка: {}'.format(error)
-
-
-
-
-@app.route('/summ/<int:first>/<znak>/<int:second>')
-def summ(first, second, znak):
-    sum = eval('{}{}{}'.format(first, znak, second))
-    return 'Сумма ваших чисел равна {}'.format(sum)
-
-
-
-#Задание 3
-@app.route('/greet/<user_name>')
-def hello(user_name):
-    return 'Hello {} '.format(user_name)
-
-app.run()
+if __name__ == '__main__':
+    app.run()
